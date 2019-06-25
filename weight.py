918850cad0f3faf73f68aa80794d3cbb472556da
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2019-06-25 16:19:30 +0800

    add cals

--- weight.py
+++ weight.py
@@ -16,14 +16,26 @@ weight = weight[['week', 'date', 'value']]
 workout = pd.read_csv('data/ActivitySummary.csv')
 workout = workout[workout.activeEnergyBurnedGoal > 0]
 workout['week'] = pd.to_datetime(workout.dateComponents).map(lambda x: 100 * x.year + x.week)
-workout = workout.groupby(by=['week'], as_index=False).appleExerciseTime.sum()
+workout = workout.groupby(by=['week'], as_index=False)[['appleExerciseTime', 'activeEnergyBurned']].sum()
 
 # merge dateset by week
 df = pd.merge(weight, workout, how='left', left_on='week', right_on='week')
 df = df.dropna()
 del df['week']
-df = df.rename({'date': 'Date', 'value': 'Weight', 'appleExerciseTime': 'ExerciseTime'}, axis=1)
+df = df.rename({'date': 'Date', 'value': 'Weight', 'appleExerciseTime': 'ExerciseTime',
+                'activeEnergyBurned': 'Calories'}, axis=1)
 df['ExerciseTime'] = df.ExerciseTime.astype('int')
+ExerciseTimeLevel = df.ExerciseTime.values
+for i in range(len(ExerciseTimeLevel)):
+    if ExerciseTimeLevel[i] <= 250:
+        ExerciseTimeLevel[i] = 250
+    elif  250 < ExerciseTimeLevel[i] <= 500:
+        ExerciseTimeLevel[i] = 500
+    elif 500 < ExerciseTimeLevel[i] <=750:
+        ExerciseTimeLevel[i] = 750
+    else:
+        ExerciseTimeLevel[i] = 1000
+df['ExerciseTimeLevel'] = ExerciseTimeLevel
 df.to_csv('myweight.csv', index=False)
 print('Output completed!')
 # # plot
